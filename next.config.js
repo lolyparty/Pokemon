7036d1d37fca05878bcf862d0dc6fc5783986d28
lolyparty <ololadeolayiwola01@gmail.com>
2022-07-08 16:34:19 +0100

    updated pokemon request and display of pokemon details

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains: ['https://pokeres.bastionbot.org','https://raw.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css'
+import colors from '../utils/theme'
+import { extendTheme } from '@chakra-ui/react'
+
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+         </ChakraProvider>
 }
 
 export default MyApp
